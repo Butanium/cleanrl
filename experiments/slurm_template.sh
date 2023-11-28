ee88ef507736fc857c26da4c756039c007928860
Clément Dumas <clement.dumas@ens-paris-saclay.fr>
2023-11-28 13:09:01 +0000

    added slurm config

---
+++ experiments/slurm_template.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=${JOB_NAME}
+#SBATCH --workdir=${PATH_TO_PROJECT_DIR}/experiments
+#SBATCH --output=./logs/slurm_${JOB_NAME}.out
+#SBATCH --error=./logs/errslurm_${JOB_NAME}.out
+#SBATCH --time=15:00:00
+#SBATCH --partition=single
+#SBATCH --gpus-per-node=1
+#SBATCH --nodes=1
+
+# Activate the env
+poetry shell
+which python
+
+# Your script goes here
